c8353029e4634197ede3cf343bfe6c0bd839af18
Eduardo Wesley <eduardomudutiu@gmail.com>
2020-05-14 15:20:41 -0300

    create controllers, Mail class, Notifications, Providers and Requests

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\Store;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    private $product;
+
+    public function __construct(Product $product)
+    {
+      $this->product = $product;
+    }
+
+    public function index()
+    {
+        $products = $this->product->limit(6)->orderBy('id', 'DESC')->get();
+        $stores = Store::limit(3)->orderBy('id', 'DESC')->get();
+
+        return view('welcome', compact('products', 'stores'));
+    }
+
+    public function single($slug)
+    {
+      $product = $this->product->whereSlug($slug)->first();
+
+      return view('single', compact('product'));
+    }
+}
